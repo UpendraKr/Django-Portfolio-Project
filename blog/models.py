9452511fc0d6cfb641b8880bb665b7e9dac84d48
Upendra Kumar <Upendra.kumar@mountblue.io>
2020-08-07 21:15:55 +0530

    bootstrap added and jobs showing

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models import TextField
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = TextField()
+    image = models.ImageField(upload_to='images/')
--- job/admin.py
+++ job/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from job.models import Job
+from blog.models import Blog
 
 admin.site.register(Job)
+admin.site.register(Blog)
